469180cf58ebd2205b1c7c4107f94ebc0e9cd697
Masinde Odera <masindeodera@gmail.com>
2020-09-10 19:37:38 +0200

    Added Method Tests in App.js

--- src/App.js
+++ src/App.js
@@ -16,13 +16,19 @@ const tempArr = [{
   email: 'joebloggs@gmail.com',
   age: 24,
   onlineStatus: true
-}]
+}];
+
+const initialState = {
+  hideBtn: false
+};
 
 class App extends Component {
-
   // eslint-disable-next-line no-useless-constructor
   constructor(props){
     super(props);
+    this.state = {
+      ...initialState
+    };
     this.fetch = this.fetch.bind(this);
   }
   /* Since this method (this.fetch) is being passed to the component,
@@ -31,10 +37,24 @@ class App extends Component {
   fetch(){
     //When this is called, dispatch action
     this.props.fetchPosts();
+    //After fetching posts, hide button
+    this.exampleMethod_updatesState();
+  }
+
+  exampleMethod_updatesState() {
+    const { hideBtn } = this.state;
+    this.setState({
+      hideBtn: !hideBtn
+    });
+  }
+
+  exampleMethod_returnsAValue(number) {
+    return number + 1;
   }
 
   render(){
     const { posts } = this.props;
+    const { hideBtn } = this.state;
 
     const configButton = {
       buttonText: 'Get posts',
@@ -46,7 +66,9 @@ class App extends Component {
         <Header />
         <section className="main">
           <Headline header="Posts" desc="Click the button to render posts!" tempArr={tempArr} />
-          <SharedButton {...configButton} />
+          {!hideBtn &&
+            <SharedButton {...configButton} />
+          }
           {posts.length > 0 && 
             <div>
               {posts.map((post, index) => {
